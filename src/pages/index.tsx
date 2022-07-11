4f7b3321c1a4db5a4fe46b794b2337dbd6e77dbb
David Mateo <davidmateo@Davids-Mac-mini.local>
2022-07-11 08:46:18 -0400

    initial version

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import { trpc } from "../utils/trpc";
+
+const Home: NextPage = () => {
+  const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
+  return <div>Hello</div>;
+};
+
+export default Home;
